8ec9cafbf10ee021967f403d18a535fadb501fca
Karolina Acosta <dianakarolinaacostaromero17@gmail.com>
2023-11-30 21:09:50 -0500

    project-1

--- my-react-task-list/src/components/TaskList.jsx
+++ my-react-task-list/src/components/TaskList.jsx
@@ -5,11 +5,22 @@ export default function TaskList(props) {
   const { title, description, priority, status } = props.taskProp;
 
   return (
-    <article className="taskCard">
-      <h2 className="subTitle">{title}</h2>
-      <p>Description:{description}</p>
-      <p>Priority:{priority}</p>
-      <p>Status:{status}</p>
+    <article>
+      <div className="taskCard">
+        <div>
+          <h2 className="sTitle">{title}</h2>
+        </div>
+        <div class="columna">
+          <div class="item">Description:</div>
+          <div class="dItem">{description}</div>
+
+          <div class="item">Priority:</div>
+          <div class="dItem">{priority}</div>
+
+          <div class="item">Status:</div>
+          <div class="dItem">{status ? "True" : "False"}</div>
+        </div>
+      </div>
     </article>
   );
 }
