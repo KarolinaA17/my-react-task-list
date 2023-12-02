5773923c51854c5dcd2abe0e206403c12c323da5
Karolina Acosta <dianakarolinaacostaromero17@gmail.com>
2023-12-01 22:05:21 -0500

    project-2

--- my-react-task-list/src/components/TaskList.jsx
+++ my-react-task-list/src/components/TaskList.jsx
@@ -1,35 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/cardTask.css";
 import DeleteButton from "./buttons/delete";
 import CompleteButton from "./buttons/complete";
 import UpdateButton from "./buttons/update";
-
 export default function TaskList(props) {
-  const { title, description, priority, status } = props.taskProp;
-
+  const { title, description, priority } = props.taskProp;
+  const [completed, setCompleted] = useState(props.taskProp.status);
+  const handleCompleteClick = () => {
+    setCompleted(!completed);
+    props.onStatusChange(!completed);
+  };
   return (
     <article>
-      <div className={status ? "taskCard completed" : "taskCard notCompleted"}>
+      <div className={`taskCard ${completed ? "completed" : "notCompleted"}`}>
         <h2 className="sTitle">{title}</h2>
-        <div className="columnaLeft">
+        <div
+          className={`columnaLeft ${
+            completed ? "completedItem" : "notCompletedItem"
+          }`}
+        >
           <div className="item">Description:</div>
           <div className="item">Priority:</div>
           <div className="item">Status:</div>
         </div>
-        <div className="columnaCenter">
+        <div
+          className={`columnaCenter ${
+            completed ? "completedDItem" : "notCompletedDItem"
+          }`}
+        >
           <div className="dItem">{description}</div>
           <div className="dItem">{priority}</div>
-          <div className="dItem">{status ? "True" : "False"}</div>
+          <div className="dItem">{completed ? "True" : "False"}</div>
         </div>
         <div className="columnaRight">
-          <div className="deleteButton">
+          <div>
             <DeleteButton />
           </div>
           <div>
             <UpdateButton />
           </div>
           <div>
-            <CompleteButton />
+            <CompleteButton
+              completed={completed}
+              onToggleComplete={handleCompleteClick}
+            />
           </div>
         </div>
       </div>
--- my-react-task-list/src/components/buttons/delete.jsx
+++ my-react-task-list/src/components/buttons/delete.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-import "../../styles/stylebuttons.css";
 
 export default function DeleteButton() {
   return (
